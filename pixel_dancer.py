5c9196aaa10c4f10828295be9bbb514786ca26dd
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-02-28 12:55:05 -0500

    implemented MVC strucutre, eliminated global variable grid_count

--- pixel_dancer.py
+++ pixel_dancer.py
@@ -1,13 +1,13 @@
 import pygame
 import numpy
 
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-BLUE = (0,0,255)
-MAX_X = 6
-MAX_Y = 6
-TOTAL_GRID = MAX_X * MAX_Y
-grid_count = 0
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+NUM_X = 6
+NUM_Y = 6
+TOTAL_GRID = NUM_X * NUM_Y
+
 
 class Grid(object):
     def __init__(self, row, col, location, length, width, color=BLACK,
@@ -30,11 +30,13 @@ class Grid(object):
     def update_grid_alpha(self, color):
         self.color = color
 
+
 class GridList(object):
     def __init__(self, num_rows, num_cols, grid_size):
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.grid_size = grid_size
+        self.colored_grid_count = 0
         length = grid_size[0] / num_cols  # length of individual grid
         width = grid_size[1] / num_rows  # width of individual grid
         grid = []
@@ -49,6 +51,7 @@ class GridList(object):
             grid.append(grid_row)
         self.gridlist = numpy.array(grid)
 
+
 class GridListViewer(object):
     def __init__(self, screen, grid_list):
         num_rows = grid_list.num_rows
@@ -58,83 +61,114 @@ class GridListViewer(object):
                 grid = grid_list.gridlist[j, i]
                 image = pygame.Surface([grid.length, grid.width])
                 image.set_alpha(grid.alpha)
-                #if (i+j) % 2 == 0:
+                # if (i+j) % 2 == 0:
                 #    color = BLACK
-                #else:
+                # else:
                 #    color = WHITE
                 image.fill(grid.color)
                 screen.blit(image, grid.location)
 
+
 class Background:
-    def __init__(self,name):
+    def __init__(self, name):
         self.pic = pygame.image.load(name)
+
+
+class BackgroundViewer:
+    def __init__(self, screen, background):
+        screen.blit(background.pic, (0, 0))
+
 
 class Player:
-    def __init__(self,name,place):
+    def __init__(self, name, place):
         self.pic = pygame.image.load(name)
-        self.pic = pygame.transform.rotozoom(self.pic,0,1/min(MAX_X,MAX_Y)*2)
-        self.place = (place[0],place[1])
-
+        self.pic = pygame.transform.rotozoom(self.pic, 0,
+                                             1/min(NUM_X, NUM_Y)*2)
+        self.place = (place[0], place[1])
         self.flipped = False
 
-    def move(self,dx,dy,gridlist):
-        global grid_count
+    def move(self, dx, dy, grid):
+        gridlist = grid.gridlist
         x = self.place[0]+dx
         y = self.place[1]+dy
         if (dx == 1 or dy == 1) and not self.flipped:
-            self.pic = pygame.transform.flip(self.pic,True,False)
+            self.pic = pygame.transform.flip(self.pic, True, False)
             self.flipped = True
         if (dx == -1 or dy == -1) and self.flipped:
-            self.pic = pygame.transform.flip(self.pic,True,False)
+            self.pic = pygame.transform.flip(self.pic, True, False)
             self.flipped = False
-        if x<0 or x>=MAX_X or y<0 or y>=MAX_Y:
+        if x < 0 or x >= NUM_X or y < 0 or y >= NUM_Y:
             x = 0
             y = 0
         if gridlist[x][y].alpha == 256:
-            gridlist[x][y].alpha /=2
+            gridlist[x][y].alpha /= 2
         elif gridlist[x][y].alpha != 0:
             gridlist[x][y].alpha = 0
-            grid_count += 1
-        self.place = (x,y)
+            grid.colored_grid_count += 1
+        self.place = (x, y)
 
-    def get_absolute_location(self,gridlist):
+    def get_absolute_location(self, gridlist):
         absolute_x = gridlist[self.place[0]][self.place[1]].location[0]
         absolute_y = gridlist[self.place[0]][self.place[1]].location[1]
-        return (absolute_x,absolute_y)
+        return (absolute_x, absolute_y)
+
+
+class PlayerKeyController():
+    def __init__(self, event, player, grid):
+        if event.key == pygame.K_RIGHT:
+            player.move(0, 1, grid)
+        if event.key == pygame.K_LEFT:
+            player.move(0, -1, grid)
+        if event.key == pygame.K_UP:
+            player.move(-1, 0, grid)
+        if event.key == pygame.K_DOWN:
+            player.move(1, 0, grid)
+
+
+class PlayerViewer:
+    def __init__(self, screen, player, grid):
+        screen.blit(player.pic, player.get_absolute_location(grid.gridlist))
+
 
 def main():
     pygame.init()
+
+    # Game Display Title : Pixel Dancer
     pygame.display.set_caption('PIXEL DANCER')
+
+    # initializing background / detriming canvas_size
     bg = Background('square.jpg')
-    player = Player('player.png',(0,0))
     canvas_size = bg.pic.get_rect().size
+
+    # initializing player
+    player = Player('player.png', (0, 0))
+
+    # initializing screen & grid & clock
     screen = pygame.display.set_mode(canvas_size)
-    grid = GridList(MAX_X, MAX_Y, canvas_size)
+    grid = GridList(NUM_X, NUM_Y, canvas_size)
     clock = pygame.time.Clock()
-    done = False
-    while not done:
+    running = True
+    while running:
+        #  checks exteral inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                done = True
+                running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    player.move(0,1,grid.gridlist)
-                if event.key == pygame.K_LEFT:
-                    player.move(0,-1,grid.gridlist)
-                if event.key == pygame.K_UP:
-                    player.move(-1,0,grid.gridlist)
-                if event.key == pygame.K_DOWN:
-                    player.move(1,0,grid.gridlist)
-        screen.blit(bg.pic,(0,0))
+                player_controller = PlayerKeyController(event, player, grid)
+
+        # initializes viewers
+        background_viewer = BackgroundViewer(screen, bg)
         grid_viewer = GridListViewer(screen, grid)
-        screen.blit(player.pic,player.get_absolute_location(grid.gridlist))
-        if grid_count == TOTAL_GRID:
+        player_viewer = PlayerViewer(screen, player, grid)
+        #  when all grids are colored with alpha value 0
+        if grid.colored_grid_count == TOTAL_GRID:
             myfont = pygame.font.SysFont("norasi", 50, True)
             label = myfont.render("THIS IS NOT OVER!", True, WHITE)
-            screen.blit(label, (50,80))
+            screen.blit(label, (50, 80))
         clock.tick(60)
         pygame.display.flip()
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
